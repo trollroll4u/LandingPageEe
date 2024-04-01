18b3676425bb66af3dc1b535af5d2636052d8c42
trollroll4u <yuvalron0@gmail.com>
2024-04-01 22:32:51 +0300

    users done

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,8 @@ import AboutUsSection from "../components/about-us-section";
 import FeaturesShow from "../components/features-show";
 import ContactUsSection from "../components/contact-us-section";
 import Footer from "../components/footer";
-import { useUser } from "@auth0/nextjs-auth0/client";
-import { useEffect } from "react";
 
 const PowerAgency: NextPage = () => {
-
-  const { user, error, isLoading } = useUser();
-
-  useEffect(() => {
-    // If the user is logged in, redirect them to the desired external URL
-    if (user) {
-      window.location.href = 'http://localhost:3001';
-    }
-  }, [user]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
   
   return (
     <div className="w-full relative [background:linear-gradient(201.96deg,_#f0f8ff,_#fff_32.5%,_#fbe6ff)] overflow-hidden flex flex-col items-start justify-start pt-[31px] px-0 pb-0 box-border gap-[10px] tracking-[normal] text-center text-xl text-main-color-1 font-poppins">
